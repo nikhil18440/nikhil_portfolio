641dfaa31a0b6468f9fd1c3570a7eb7c066a882e
nikhil <nickjr1032@gmail.com>
2025-03-02 16:05:15 +0530

    made hero page

--- app/componants/navbar/Navbar.jsx
+++ app/componants/navbar/Navbar.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+
+import React from 'react';
+import styles from './navbar.module.css';
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>My Portfolio</div>
+      <ul className={styles.navLinks}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import heroStyles from "./componants/hero/hero.module.css";
+import Navbar from "./componants/navbar/Navbar";
 
 export default function Home() {
   return (
     <div className={styles.Home}>
-      portfolio
+      <Navbar />
+      <div className={heroStyles.heroSection}>
+        <h1 className={heroStyles.heroTitle}>Welcome to My Portfolio</h1>
+        <p className={heroStyles.heroSubtitle}>A glimpse into my projects and skills.</p>
+        <button className={heroStyles.heroButton}>View Projects</button>
+      </div>
+      <div className={heroStyles.heroBackground}></div>
     </div>
   );
 }
